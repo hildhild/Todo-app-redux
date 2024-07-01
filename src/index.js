254cceb95847ad671a01c557ab3e84ff53628123
hildhild <huy.ledinh@hcmut.edu.vn>
2024-07-01 23:25:27 +0700

    finish

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './redux/store';
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
     <Provider store = {store}>
         <App />
-    </Provider>, 
+    </Provider>
+  </React.StrictMode>
 );
